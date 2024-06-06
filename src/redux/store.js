b912175e130aeb517e11443fc1e3a0cc205022f3
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-06-07 00:03:41 +0300

    xcv

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
-import {
-   persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 
 
 
-const persistedBalanceReducer = persistReducer(
-  {
-    key: 'users',
-    storage,
-  },
-  contactsReducer
-);
+
+
 
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedBalanceReducer,
-  },  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+    contacts: contactsReducer,
+  }, 
 });
 
 
-export const persistor = persistStore(store);
+
